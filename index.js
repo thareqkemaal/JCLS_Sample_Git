6c9d5b0dd09942e30a35536fa6c9964b185a6680
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-06-17 15:02:40 +0700

    Added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello Git")
+// apabila membuat file baru, file tsb akan berwarna hijau oleh vscode
+// dan terdapat kode U = Untracked
+// pada source control, untuk file yang baru dan sedang diedit akan masuk ke changes
+// jika ada file yang sudah selesai harus di pindahkan ke staging
+// untuk memindahkan ke staging, di terminal diketik ' git add . ' jika banyak file, kalo cm 1 ' git add namafile ' 
+// file tsb akan di area branch => folder utama dari project, secara default branch akan diarahkan ke main folder
+// untuk pindah dari staging ke branch => menggunakan ' git commit -m "isi pesan catatan" '
